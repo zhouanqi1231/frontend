a5b077664c24c6be5e51c2126e6796f20b498c70
zhouanqi <angiechou1231@gmail.com>
2023-04-20 14:13:19 +0800

    canvas 2 svg

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+// Copyright 2022 Takanori Fujiwara.
+// Released under the BSD 3-Clause 'New' or 'Revised' License
+
+import { forceGraph } from "./chart.js";
+
+// const data = await d3.json("./data/miserables.json");
+
+// get json response from backend
+const httpRequest = new XMLHttpRequest();
+httpRequest.open("GET", "http://127.0.0.1:5000/query_all", true);
+httpRequest.send();
+
+// 接受到response之后（左侧变量发生改变），右侧函数被回调
+httpRequest.onreadystatechange = function () {
+  // store response json
+  let data;
+  if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+    // get response (json but in string)
+    const queryAllRes = httpRequest.responseText;
+    data = JSON.parse(queryAllRes);
+    console.log(data);
+  } else {
+    return;
+  }
+
+  const chart = forceGraph(data, {
+    nodeId: (d) => d.node_id,
+    nodeGroup: (d) => d.label,
+    nodeTitle: (d) => `${d.node_id}\n${d.label}`,
+    // linkStrokeWidth: (l) => Math.sqrt(l.value),
+    width: window.innerWidth,
+    height: window.innerHeight,
+    nodeStrength: -10,
+    nodeRadius: (d) => d.value,
+  });
+
+  d3.select("body").append(() => chart);
+};
